931dbda706735567003c06928bacedfed7edc054
gdamelin <66569945+gdamelin@users.noreply.github.com>
2021-11-10 14:52:23 -0800

    Split up method

--- TheTechSpecProgram/src/AuctionUser.java
+++ TheTechSpecProgram/src/AuctionUser.java
@@ -5,31 +5,36 @@ public class AuctionUser
 {
 	public Wallet wallet;
 	public ArrayList <NFT> nftList = new ArrayList<NFT>();
-	
+
 	public AuctionUser (Wallet wallet)
 	{
 		this.wallet = wallet;
 	}
-	
+
 	public void auction (NFT nft)
 	{
 		nftList.add(nft);
 	}
-	
+
 	public void buyNFT (NFT nft, double offer)
 	{
 		if (nft != null)
 		{
 			if (offer >= nft.price)
 			{
-				wallet.money = wallet.money - nft.price;
-				nft.transferOwner(wallet);
-				nft.price = offer;
+				completeNFTProcess (nft, offer);
 			}
 		}
 		else
 		{
 			System.out.println ("Oops. This NFT doesn't exist.");
 		}
+	}
+
+	public void completeNFTProcess (NFT nft, double offer)
+	{
+		wallet.money = wallet.money - nft.price;
+		nft.transferOwner(wallet);
+		nft.price = offer;
 	}
 }
